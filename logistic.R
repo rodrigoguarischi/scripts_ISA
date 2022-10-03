bdbbdc8e635c2b126f2546d4516e8eb0e2240412
camila-alves <milaalvesdesouza@gmail.com>
2022-10-02 22:41:15 -0300

    Atualizando analise de regressao

--- logistic.R
+++ logistic.R
@@ -2,14 +2,16 @@ library(ggplot2)
 library(dplyr)
 library(MASS)
 
-data <- read.delim("C:/Users/Camila/OneDrive/Área de Trabalho/Ánalises/banco707.txt")
-
+data <- read.delim("C:/Users/Camila/OneDrive/Área de Trabalho/Análises/banco707.txt")
 head(data)
-dta <- data.frame(data$samplefilename,data$computed_gender,data$idade,
+
+dta <- data.frame(data$samplefilename,
+                  data$sexo,
+                  data$faixa_etaria,
                   data$raca_cor,
                   data$imc,
                   data$obesidade,
-                  data$CP1,data$CP2)
+                  data$idade)
 head(dta)
 
 barplot(data.frame(table(dta$data.obesidade))[,2], 
@@ -20,27 +22,37 @@ barplot(data.frame(table(dta$data.obesidade))[,2],
 
 ###############################
 
-new <- read.delim("C:/Users/Camila/OneDrive/Área de Trabalho/Ánalises/loh707.txt")
+new <- read.delim("C:/Users/Camila/OneDrive/Área de Trabalho/Análises/loh707.txt")
 head(new)
+cps <- read.delim("C:/Users/Camila/OneDrive/Área de Trabalho/Análises/cps.txt")
+head(cps)
 
-df= dta %>% inner_join(new,by="data.samplefilename")
+df_b = dta %>% inner_join(new,by="data.samplefilename")
+df = df_b %>% inner_join(cps,by="data.samplefilename")
 head(df)
 
 ggplot(df, aes(x=LOH, y=data.obesidade)) + 
   geom_point() + 
   ggtitle("Obesidade e Porcentagem de LOH") +
   xlab("% de LOH nos Autossômicos") + ylab("Obesidade (sim=1 e não=0)")
-  #stat_smooth(method="glm", method.args=list(family="binomial"), se=FALSE)
 m2=glm(data.obesidade~LOH, family = binomial(link="logit"), data = df)
-summary(m2)$coef
+summary(m2)
 
 #######################################################
 
-m3=glm(data.obesidade~LOH + data.computed_gender + data.idade,
+idade <- c()
+mean_idade <- mean(df$data.idade)
+for (i in 1:length(df$data.idade)) {
+  idade[i] <- df$data.idade[i]-mean_idade
+}
+
+m3=glm(data.obesidade ~ CP1 + CP2 + data.sexo + 
+         idade + data.sexo*idade + LOH,
        family = binomial(link="logit"), data = df)
-summary(m3)$coef
+summary(m3)
 confint(m3)
 
 step.model <- stepAIC(m3, direction = "both", trace = FALSE)
 summary(step.model)
 
+#########################################################
